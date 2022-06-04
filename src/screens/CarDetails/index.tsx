5ae5270011e8e230e2daaa8c04f7a9dd219cc133
WAY2\everton.oliveira <everton.oliveira@way2.com.br>
2022-06-04 13:47:27 -0300

    Continuando a interfcae de detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,24 +1,52 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CardImage} from "./styles";
+import {
+  Container,
+  Header,
+  CardImage,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
 export function CarDetails() {
   return (
     <Container>
-      <Header>
-        {/* <BackButton onPress={() => {}} /> */}
-      </Header>
+      <Header>{/* <BackButton onPress={() => {}} /> */}</Header>
       <CardImage>
-      <ImageSlider
-        imagesUrl={[
-          "https://cdn.motor1.com/images/mgl/z2or8/s1/test-audi-rs-5-sportback-2020-facelift.jpg",
-        ]}
-      />
+        <ImageSlider
+          imagesUrl={[
+            "https://cdn.motor1.com/images/mgl/z2or8/s1/test-audi-rs-5-sportback-2020-facelift.jpg",
+          ]}
+        />
       </CardImage>
-      
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Ferrari</Brand>
+            <Name>Ferrari nova</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 120</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          O empenho em analisar a contínua expansão de nossa atividade promove a
+          alavancagem dos conhecimentos estratégicos para atingir a excelência.
+        </About>
+      </Content>
     </Container>
   );
 }
