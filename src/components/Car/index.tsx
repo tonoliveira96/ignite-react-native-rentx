1a01d3bf104e6f5ea273f45b81ecbbb7060c0b31
WAY2\everton.oliveira <everton.oliveira@way2.com.br>
2022-06-04 12:08:31 -0300

    Criação do Card Car

---
+++ src/components/Car/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import GasolineSVG from '../../assets/gasoline.svg';
+
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CardImage,
+} from "./styles";
+
+interface CarData {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: number;
+  };
+  thumbnail: string;
+}
+
+interface Props {
+  data: CarData;
+}
+
+export function Car({ data }: Props) {
+
+  return (
+    <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>{`R$ ${data.rent.price}`}</Price>
+          </Rent>
+
+          <Type>
+            <GasolineSVG />
+          </Type>
+        </About>
+      </Details>
+
+      <CardImage
+        source={{
+          uri: data.thumbnail
+        }}
+        resizeMode="contain"
+      />
+    </Container>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,31 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
-import { Container, Header,HeaderContent, TotalCars } from "./styles";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home() {
+  const carData = {
+    thumbnail:
+      "https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png",
+    brand: "audi",
+    name: "RS 5 couple",
+    rent: {
+      period: "ao dia",
+      price: 120,
+    },
+  };
+  const carData2 = {
+    thumbnail:
+      "https://cdn.motor1.com/images/mgl/z2or8/s1/test-audi-rs-5-sportback-2020-facelift.jpg",
+    brand: "audi",
+    name: "RS 5 couple",
+    rent: {
+      period: "ao dia",
+      price: 120,
+    },
+  };
   return (
     <Container>
       <StatusBar
@@ -16,13 +37,13 @@ export function Home() {
       />
       <Header>
         <HeaderContent>
-        <Logo width={RFValue(108)} height={RFValue(12)} />
-        <TotalCars>
-          Total de 12 carros
-        </TotalCars>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
-       
       </Header>
+
+      <Car data={carData} />
+      <Car data={carData2} />
     </Container>
   );
 }
